a61d98ed56a9dfa4ecc6709a4a322d5a0759e300
Jacob A Brolliar <jabrolliar@ecc-jaguar.csuchico.edu>
2019-11-21 16:26:21 -0800

    fixed functions

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -74,35 +74,31 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  Piece winner = Invalid;
   if(board[0][0] == board[0][1] == board[0][2]){
-    winner = board[0][0];
+    return = board[0][0];
   }else if(board[1][0] == board[1][1] == board[1][2]){
-    winner = board[1][0];
+    return = board[1][0];
   }else if(board[2][0] == board[2][1] == board[2][2]){
-    winner = board[2][0];
+    return = board[2][0];
   }else if(board[0][0] == board[1][0] == board[2][0]){
-    winner = board[0][0];
+    return = board[0][0];
   }else if(board[0][1] == board[1][1] == board[2][1]){
-    winner = board[0][1];
+    return = board[0][1];
   }else if(board[0][2] == board[1][2] == board[2][2]){
-    winner = board[0][2];
+    return = board[0][2];
   }else if(board[0][0] == board[1][1] == board[2][2]){
-    winner = board[0][0];
+    return = board[0][0];
   }else if(board[0][2] == board[1][1] == board[2][0]){
-    winner = board[0][2];
+    return board[0][2];
   }
   
   for(int i = 0; i < 3; i++){
     for(int j = 0; j < 3; j++){
       if(board[i][j] == ' '){
-        return winner;
+        return Invalid;
       }
     }
   }
   
-  if(winner == X || winner == O){
-    return winner;
-  }
   return Blank;
 }
