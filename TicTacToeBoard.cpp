4fdf6e3fc412ae15ab77291ef236c6db364396c3
Jacob A Brolliar <jabrolliar@ecc-jaguar.csuchico.edu>
2019-11-21 17:19:49 -0800

    added to conditionals

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -82,24 +82,22 @@ Piece TicTacToeBoard::getWinner()
       }
     }
   }
-  if((board[0][0] == Blank) || (board[1][1] == Blank) || (board[2][2] == Blank)){
-    return win;
-  }
-  if((board[0][0] == board[0][1]) && (board[0][0] == board[0][2])){
+  
+  if((board[0][0] == board[0][1]) && (board[0][0] == board[0][2]) && (board[0][0] != Blank)){
     return board[0][0];
-  }else if((board[1][0] == board[1][1]) && (board[1][0] == board[1][2])){
+  }else if((board[1][0] == board[1][1]) && (board[1][0] == board[1][2]) && (board[1][0] != Blank)){
     return board[1][0];
-  }else if((board[2][0] == board[2][1]) && (board[2][0] == board[2][2])){
+  }else if((board[2][0] == board[2][1]) && (board[2][0] == board[2][2]) && (board[2][0] != Blank)){
     return board[2][0];
-  }else if((board[0][0] == board[1][0]) && (board[0][0] == board[2][0])){
+  }else if((board[0][0] == board[1][0]) && (board[0][0] == board[2][0]) && (board[0][0] != Blank)){
     return board[0][0];
-  }else if((board[0][1] == board[1][1]) && (board[0][1] == board[2][1])){
+  }else if((board[0][1] == board[1][1]) && (board[0][1] == board[2][1]) && (board[0][1] != Blank)){
     return board[0][1];
-  }else if((board[0][2] == board[1][2]) && (board[0][2] == board[2][2])){
+  }else if((board[0][2] == board[1][2]) && (board[0][2] == board[2][2]) && (board[0][2] != Blank)){
     return board[0][2];
-  }else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2])){
+  }else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) && (board[0][0] != Blank)){
     return board[0][0];
-  }else if((board[0][2] == board[1][1]) && (board[0][2]== board[2][0])){
+  }else if((board[0][2] == board[1][1]) && (board[0][2]== board[2][0]) && (board[0][2] != Blank)){
     return board[0][2];
   }
   
