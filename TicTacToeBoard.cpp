acf07db33f423167997868963344cc2be279f06b
Jacob A Brolliar <jabrolliar@ecc-jaguar.csuchico.edu>
2019-11-21 16:14:04 -0800

    fixed get functions

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -94,7 +94,7 @@ Piece TicTacToeBoard::getWinner()
   }
   
   for(int i = 0; i < 3; i++){
-    for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
       if(board[i][j] == ' '){
         return winner;
       }
