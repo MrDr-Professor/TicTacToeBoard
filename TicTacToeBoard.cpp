6b19941798314b2a060f0391de8b2dd6134fc231
Jacob A Brolliar <jabrolliar@ecc-jaguar.csuchico.edu>
2019-11-21 17:13:48 -0800

    fixed index

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -99,7 +99,7 @@ Piece TicTacToeBoard::getWinner()
     return board[0][2];
   }else if((board[0][0] == board[1][1]) && (board[0][0] == board[2][2])){
     return board[0][0];
-  }else if((board[0][2] == board[1][1]) && (board[0][0]== board[2][0])){
+  }else if((board[0][2] == board[1][1]) && (board[0][2]== board[2][0])){
     return board[0][2];
   }
   
